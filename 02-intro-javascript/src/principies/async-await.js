3a288cbd9754fb6299c6022217f3fc222580b55e
Carlos Araiza <eliptikman1234@gmail.com>
2021-01-06 13:38:32 -0600

    Updated async and await file

--- 02-intro-javascript/src/principies/async-await.js
+++ 02-intro-javascript/src/principies/async-await.js
@@ -1,5 +1,3 @@
-
-
 const gif = async ()=> {
     try{
         const apikey = "eNaOTPt3KxCMPvbP1317OYZ2YCbzexFR";
@@ -17,25 +15,3 @@ const gif = async ()=> {
     }
 
 }
-
-
-gif();
-
-
-/*
-const apikey = "eNaOTPt3KxCMPvbP1317OYZ2YCbzexFR";
-
-const petition = fetch(`http://api.giphy.com/v1/stickers/random?api_key=${apikey}`);
-
-petition.then(res=>{
-    return res.json();
-}).then( ({data}) =>{
-    const url = data.image_original_url;
-
-    const img = document.createElement("img");
-    img.src = url;
-
-    document.body.append(img);
-
-}).catch(console.log);
-*/
