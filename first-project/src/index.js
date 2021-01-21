338582db935bfc33645a6f42a43f2f1eb15d01ae
Carlos Araiza <eliptikman1234@gmail.com>
2021-01-21 15:06:51 -0600

    Create a new React project

---
+++ first-project/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Greetings = () => {
+  return(
+    <h1>Hello World!! Yoo</h1>
+  );
+};
+
+
+ReactDOM.render(<Greetings/>,document.querySelector('#root'));
