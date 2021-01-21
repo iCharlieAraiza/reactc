a03a6f82319780ac07e0964e6c4daf52ef8bf9db
Carlos Araiza <eliptikman1234@gmail.com>
2021-01-21 15:57:13 -0600

    Create counter component

--- first-project/src/index.js
+++ first-project/src/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Menu from './components/menu/menu';
+import PropTypes from 'prop-types';
+import Counter from './components/counter/counter';
 
 const Greetings = () => {
   return(
-    <h1>Hello World!! Yoo</h1>
-  );
-};
+    <>
+      <Menu name={77}/>
+      <h1>Hello World!! Yoo</h1>
+      <Counter/>
+    </>
+  )
+}
 
+Greetings.propTypes = {
+  name: PropTypes.string.isRequired
+}
 
 ReactDOM.render(<Greetings/>,document.querySelector('#root'));
