348ec510486418e513dadcd0e927f19b90f5b657
Carlos Araiza <eliptikman1234@gmail.com>
2021-03-03 02:18:01 -0600

    First jest test

--- counter-app/src/tests/alga.test.js
+++
@@ -1,3 +0,0 @@
-test('should ', () => {
-    console.log("LHoal");
-})
--- counter-app/src/tests/hello.test.js
+++ counter-app/src/tests/hello.test.js
@@ -1,3 +1,16 @@
-test('should ', () => {
+
+describe( "Pruebas en el archivo hello.test.js", ()=>{
+    test('deben ser iguales los strings ', () => {
+
+        const isActive = true;
     
-})
+        const mensage = 'Hola mundo';
+    
+        const mensage2 = "Hola mundo";
+    
+        expect(mensage).toBe(mensage2);
+    
+    })
+    
+});
+
