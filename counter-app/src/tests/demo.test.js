8e74b2ad02de902e4acef1844e61b307f1fe7f8e
Carlos Araiza <eliptikman1234@gmail.com>
2021-05-17 11:22:06 -0500

    Add test files

--- counter-app/src/tests/demo.test.js
+++ counter-app/src/tests/demo.test.js
@@ -1,13 +1,18 @@
-test('the stringgs should be equals', ()=>{
+//Test Wrapper
+describe('Test of all', ()=>{
+
+    //Test
+    test('the stringgs should be equals', ()=>{
     
-    // 1. Arrange: Init element
-    const message = 'Hello World';
+        // 1. Arrange: Init element
+        const message = 'Hello World';
+    
+        //2. Act
+        const message2 = `Hello World`;
+    
+        //3. Assert
+        expect( message ).toBe( message2 );
+    
+    });
 
-    //2. Act
-    const message2 = `Hello World`;
-
-    //3. Assert
-    expect(message).toBe(message2);
-
-
-});
+})
